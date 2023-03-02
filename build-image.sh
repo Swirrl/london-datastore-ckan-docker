f784968cd2d2fcc597804643cff5d3f9e204c03e
Kiran Joshi <kiran@swirrl.local>
2023-03-02 10:18:25 +0000

    Update build script

--- build-image.sh
+++ build-image.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
 BUILD_DIR=build_image
-EXTENSIONS=("ckanext-gla_theme")
+EXTENSIONS_DIR=$BUILD_DIR/extensions
+# EXTENSIONS=("ckanext-gla_theme")
 
-mkdir $BUILD_DIR
+mkdir -p $EXTENSIONS_DIR
 
 # Copy the CKAN base stuff
 cp -r ckan/* $BUILD_DIR
 
-# Copy the EXTENSIONS
-for e in ${EXTENSIONS[@]}; do
-  cp -r src/$e $BUILD_DIR/extensions
-done
-# Build the CKAN image
+# Copy all extensions from the src/ directory into the EXTENSIONS_DIR
+cp -r src/ $EXTENSIONS_DIR
 
+# Build the CKAN image
 pushd $BUILD_DIR
+tree -L 3
 make build
 popd
 
 rm -r $BUILD_DIR
-
